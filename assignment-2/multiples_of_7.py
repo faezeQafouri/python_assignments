a12be0b441f223f51cd9cb1d17483e9b1289c80e
faeze Qafouri <110340213+faezeQafouri@users.noreply.github.com>
2022-08-24 11:13:27 +0430

    Add files via upload

---
+++ assignment-2/multiples_of_7.py
@@ -0,0 +1,22 @@
+
+
+# if num % 7 == 0  print true 
+# else Find the first 7-dimensional multiple of that number
+
+import os 
+os.system('cls')
+
+
+num = int (input("Enter a number:"))
+temp = num 
+
+if num % 7 == 0 :
+    print ("The number you entered is: " + str(num) + "  and its multiple of 7 :D ")
+    
+
+else :
+     num1  = num // 7
+     num2 = (num1*7)+7
+    
+     print("The number you Enter is: " + str(temp) +"\tIts not in multiples of 7 !! " ,"\nI showed the nearest number that was a multiple of 7:" , num2 )
+        

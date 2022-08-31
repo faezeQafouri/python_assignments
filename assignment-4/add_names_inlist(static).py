de5008352c76001a546da8adc463f3d9df8b81c0
faeze Qafouri <110340213+faezeQafouri@users.noreply.github.com>
2022-08-31 20:31:31 +0430

    Add files via upload

---
+++ assignment-4/add_names_inlist(static).py
@@ -0,0 +1,16 @@
+
+
+names = [ 'ali','atefe','homa','amir','fateme']
+
+
+for i in range (1,4):
+
+    element = input("\bEnter a names you want to add :")
+    
+    # Element to be inserted before
+    beforeElement = int(input("\bwhich number do you want in queue  :"))
+    
+
+    names.insert(beforeElement-1,element) 
+    print('\t',names)
+         
